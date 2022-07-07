f98ef6edb73f93f04c92cdca5d9af6d008832bb7
Mikndesu <mi22kknsinge@ezweb.ne.jp>
2022-07-07 23:30:08 +0900

    parse commandline args and put them into struct

--- download_vimeo/src/main.rs
+++ download_vimeo/src/main.rs
@@ -2,13 +2,13 @@ use download_vimeo::commandline::parser;
 
 extern crate clap;
 
+pub struct Params {
+    url: String,
+    output_path: String
+}
+
 #[tokio::main]
 async fn main() {
     let matches = parser::Parser::origin().command.get_matches();
-    if let Some(o) = matches.value_of("url") {
-        println!("{}", o);
-    }
-    if let Some(o) = matches.value_of("output") {
-        println!("{}", o);
-    }
+    let params = Params{url:matches.value_of("url").unwrap().to_string(), output_path:matches.value_of("output").unwrap().to_string()};
 }
